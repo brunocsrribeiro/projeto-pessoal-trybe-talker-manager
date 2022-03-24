6df0d37ee10f9ccf3fd2b923ff83d0d4a0151122
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-03-24 10:19:50 -0300

    feat: Requisito getAllTalkers concluido

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const talkerJSON = './talker.json';
+const encoding = 'utf-8';
+
+app.get('/talker', async (_req, res) => {
+  const talkerFS = await fs.readFile(talkerJSON, encoding)
+    .then((fileName) => JSON.parse(fileName))
+    .catch((err) => res.status(200).json(err));
+  res.status(200).json(talkerFS);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
